820ead1d30632b3f590234237e2c644718f768e7
sunu2018 <39335771+sunu2018@users.noreply.github.com>
2018-06-08 20:11:57 +0700

    ...

--- gi4.py
+++ gi4.py
@@ -63,42 +63,42 @@ gye.log("Channel Token : " + str(channelToken))
 #ki3 = LINE("EtDOOeYj4Rvl5PVfOEaa.ETpCu8czFapUIJQDqIA82G.tcOaI+VmHhWwMbyDL/7yXupWfdIvUJh80yWzu/UJXp8=")
 #ki4 = LINE("EtWyu42OHWKSaxPHY3yd.jTri3xzV4E2Z1xvWxjTrRq.s1oy5gbYMT2haZV7l6yzV0bp5gONcnu+bGSSJ1mbT0c=")
 
-KAC = [gye,ais,ki2,ki3,ki4]
-GUE = [ais,ki2,ki3,ki4] # ini jangan luh hapus peak ini fungsi Ciak alias kick
+KAC = [gye]#,ais,ki2,ki3,ki4]
+GUE = [ais]#,ki2,ki3,ki4] # ini jangan luh hapus peak ini fungsi Ciak alias kick
 #maksudnya agar bot sb/induk gak ikutan nge kick Mudeng ora
 gyeMID = gye.profile.mid
 aisMID = ais.profile.mid
 ki2MID = ki2.profile.mid
 ki3MID = ki3.profile.mid
 ki4MID = ki4.profile.mid
-Bots = [gyeMID,aisMID,ki2MID,ki3MID,ki4MID] #ini jangan dinrubah Gunanya agar bot tidak saling kick
+Bots = [gyeMID]#,aisMID,ki2MID,ki3MID,ki4MID] #ini jangan dinrubah Gunanya agar bot tidak saling kick
 creator = ["u104e95aaefb53cf411f77353f6a96ece"]
 Owner = ["u104e95aaefb53cf411f77353f6a96ece"]
 admin = ["u104e95aaefb53cf411f77353f6a96ece"]
 
 gyeProfile = gye.getProfile()
-aisProfile = ais.getProfile()
-ki2Profile = ki2.getProfile()
-ki2Profile = ki3.getProfile()
-ki2Profile = ki4.getProfile()
+#aisProfile = ais.getProfile()
+#ki2Profile = ki2.getProfile()
+#ki2Profile = ki3.getProfile()
+#ki2Profile = ki4.getProfile()
 
 lineSettings = gye.getSettings()
-aisSettings = ais.getSettings()
-ki2Settings = ki2.getSettings()
-ki3Settings = ki3.getSettings()
-ki4Settings = ki4.getSettings()
+#aisSettings = ais.getSettings()
+#ki2Settings = ki2.getSettings()
+#ki3Settings = ki3.getSettings()
+#ki4Settings = ki4.getSettings()
 
 oepoll = OEPoll(gye)
-oepoll1 = OEPoll(ais)
-oepoll2 = OEPoll(ki2)
-oepoll3 = OEPoll(ki3)
-oepoll4 = OEPoll(ki4)
+#oepoll1 = OEPoll(ais)
+#oepoll2 = OEPoll(ki2)
+#oepoll3 = OEPoll(ki3)
+#oepoll4 = OEPoll(ki4)
 
 responsename = gye.getProfile().displayName
-responsename2 = ais.getProfile().displayName
-responsename3 = ki2.getProfile().displayName
-responsename2 = ki3.getProfile().displayName
-responsename3 = ki4.getProfile().displayName
+#responsename2 = ais.getProfile().displayName
+#responsename3 = ki2.getProfile().displayName
+#responsename2 = ki3.getProfile().displayName
+#responsename3 = ki4.getProfile().displayName
 #==============================================================================#
 
 
