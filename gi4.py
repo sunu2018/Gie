e0ce8d743513a22da87427e0bc27ff09425bf47d
sunu2018 <39335771+sunu2018@users.noreply.github.com>
2018-06-09 21:21:21 +0700

    ..

--- gi4.py
+++ gi4.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from time import sleep
 from bs4 import BeautifulSoup
 from humanfriendly import format_timespan, format_size, format_number, format_length
-import time, random, sys, json, codecs, threading, glob, re, string, os, requests, subprocess, six, ast, pytz, urllib, urllib.parse
+#import time, random, sys, json, codecs, threading, glob, re, string, os, requests, subprocess, six, ast, pytz, urllib, urllib.parse
 from gtts import gTTS
 from googletrans import Translator
  
