57156c983b501d2080252cf4a8946677f637e8a9
sunu2018 <39335771+sunu2018@users.noreply.github.com>
2018-06-09 22:10:52 +0700

    .

--- gi4.py
+++ gi4.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from time import sleep
 from bs4 import BeautifulSoup
 from humanfriendly import format_timespan, format_size, format_number, format_length
-#import time, random, sys, json, codecs, threading, glob, re, string, os, requests, subprocess, six, ast, pytz, urllib, urllib.parse
+import time, random, sys, json, codecs, threading, glob, re, string, os, requests, subprocess, six, ast, pytz, urllib, urllib.parse
 from gtts import gTTS
 from googletrans import Translator
  
@@ -25,7 +25,7 @@ from googletrans import Translator
 # Jangan Lupa Add Admin 
 # id Line ( aisyagye )
 #==============================================================================#
-#botStart = time.time()
+botStart = time.time()
 #kalo mau login code qr disini pake
 #gye = LINE()
 #gye.log("Auth Token : " + str(gye.authToken))
